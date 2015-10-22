6337bace8965fcc930830d0dc40f31cfa90c0743
koontz <koontz@cs.colostate.edu>
2015-10-21 22:56:49 -0600

    header completed. Checking how it looks at school

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $pageDepth = 0;
 <?php include "common/navigation-row.php" ?>
 
 <div class="row clearfix">
-    <div class="column full landing animated fadeInUp">
-
+    <div class="column full landing">
+        <p>hi</p>
     </div>
 </div>
 <!--row-->
@@ -20,27 +20,23 @@ $pageDepth = 0;
 <div class="row clearfix">
     <div class="column full social">
         <div class="social-media-list">
-            <a href="https://instagram.com/jaredkoontz" target="_blank" class="fa fa-github"></a>
+            <a href="https://github.com/jaredkoontz" target="_blank" class="fa fa-github"></a>
             <a href="https://linkedin.com/in/jaredkoontz" target="_blank" class="fa fa-linkedin"></a>
         </div>
     </div>
 </div>
 <!--row-->
 
+
+
 <div class="row clearfix">
     <div class="copyright column full">
         <hr>
-        <h5>© Lauren Nicole Smith 2011-2014</h5>
+        <h5>© Jared Koontz 2015</h5>
     </div>
     <!--copyright-->
 </div>
 <!--row-->
-
-<div class="row clearfix">
-
-    <div class="baseline column full"></div>
-
-</div>
 
 </body>
 </html>
